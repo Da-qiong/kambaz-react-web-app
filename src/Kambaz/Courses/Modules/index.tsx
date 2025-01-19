da34d9b29e5dcb3cb6608419260c4016847739e1
Da-qiong <qls2192729905@outlook.com>
2025-01-19 16:18:20 -0500

    Update index.tsx

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -12,22 +12,36 @@ export default function Modules() {
       <button>+ Module</button>
         <ul id="wd-modules">
             <li className="wd-module">
-                <div className="wd-title">Week 1</div>
+                <div className="wd-title"><strong>Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</strong></div>
                 <ul className="wd-lessons">
                     <li className="wd-lesson">
-                        <span className="wd-title">LEARNING OBJECTIVES</span>
+                        <span className="wd-title"><strong>LEARNING OBJECTIVES</strong></span>
                         <ul className="wd-content">
-                            <li className="wd-content-item">Introduction to the course</li>
-                            <li className="wd-content-item">Learn what is Web Development</li>
+                            <li className="wd-content-item"><strong>Introduction to the course</strong></li>
+                            <li className="wd-content-item"><strong>Learn what is Web Development</strong></li>
                         </ul>
+
+                        <span className="wd-title"><strong>READING</strong></span>
+                        <ul className="wd-content">
+                            <li className="wd-content-item"><strong>Full Stack Developer - Chapter 1 - Introduction</strong></li>
+                            <li className="wd-content-item"><strong>Full Stack Developer - Chapter 2 - Creating User</strong></li>
+                        </ul>
+
+                        <span className="wd-title"><strong>SLIDES</strong></span>
+                        <ul className="wd-content">
+                            <li className="wd-content-item"><strong>Introduction to Web Development</strong></li>
+                            <li className="wd-content-item"><strong>Creating an HTTP server with Node.js</strong></li>
+                            <li className="wd-content-item"><strong>Creating a React Application</strong></li>
+                        </ul>
+
             </li>
         </ul>
         </li>
             <li className="wd-module">
-                <div className="wd-title">Week 2</div>
+                <div className="wd-title"><strong>Week 1, Lecture 2 - Formatting User Interfaces with HTML</strong></div>
             </li>
             <li className="wd-module">
-                <div className="wd-title">Week 3</div>
+                <div className="wd-title"><strong>Week 2</strong></div>
             </li>
         </ul>
     </div>
