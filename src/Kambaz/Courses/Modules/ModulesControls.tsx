e4944b29d567573936cfc5f1687e5793d7847eb3
Da-qiong <qls2192729905@outlook.com>
2025-03-29 22:33:20 -0400

    Update ModulesControls.tsx

--- src/Kambaz/Courses/Modules/ModulesControls.tsx
+++ src/Kambaz/Courses/Modules/ModulesControls.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import ModuleEditor from "./ModuleEditor";
@@ -6,27 +7,37 @@ export default function ModulesControls(
     { moduleName, setModuleName, addModule }:
         { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }
 ) {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+    if (currentUser.role !== "FACULTY") {
+        return null;
+    }
+
     return (
         <div id="wd-modules-controls" className="text-nowrap">
             <button className="btn btn-lg btn-danger me-1 float-end" id="wd-add-module-btn"
                 data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog" >
                 <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Module</button>
+                Module
+            </button>
             <div className="dropdown d-inline me-1 float-end">
                 <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
                     type="button" data-bs-toggle="dropdown">
                     <GreenCheckmark />
-                    Publish All</button>
+                    Publish All
+                </button>
                 <ul className="dropdown-menu">
                     <li>
                         <a id="wd-publish-all-modules-and-items-btn" className="dropdown-item" href="#">
                             <GreenCheckmark />
-                            Publish all modules and items</a>
+                            Publish all modules and items
+                        </a>
                     </li>
                     <li>
                         <a id="wd-publish-modules-only-button" className="dropdown-item" href="#">
                             <GreenCheckmark />
-                            Publish modules only</a>
+                            Publish modules only
+                        </a>
                     </li>
                     <li>
                         <a id="wd-unpublish-all-modules-and-items" className="dropdown-item" href="#">
@@ -45,7 +56,6 @@ export default function ModulesControls(
             <button id="wd-view-progress" className="btn btn-lg btn-secondary me-1 float-end">
                 View Progress
             </button>
-
             <button id="wd-collapse-all" className="btn btn-lg btn-secondary me-1 float-end">
                 Collapse All
             </button>
